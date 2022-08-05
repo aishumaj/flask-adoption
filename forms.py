8f4f9bc7fdc239beec21086772f5ef92a151d257
Aishwarya Majumdar <aishu.maj1996@gmail.com>
2022-08-05 12:34:33 -0700

    added a comma i missed

--- forms.py
+++ forms.py
@@ -10,7 +10,7 @@ class AddPetForm(FlaskForm):
 
     name = StringField("Pet Name",
         validators = [InputRequired()])
-    species = SelectField("Species"
+    species = SelectField("Species",
         choices = [("cat", "Cat"), ("dog", "Dog"), ("porcupine", "Porcupine")])
     photo_url = StringField("Photo URL",
         validators= [Optional(), URL()])
